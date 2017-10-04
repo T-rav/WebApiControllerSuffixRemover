0a06d17aac55397106e82cc9ddc3ab5b7eeb4402
T-rav <travis.frisinger@chillisoft.co.za>
2017-10-04 12:29:50 +0200

    Started adding code for this

---
+++ source/WebApi.ControllerSuffixRemover/WebApi.ControllerSuffixRemover/CustomHttpControllerTypeResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WebApi.ControllerSuffixRemover
+{
+    public class CustomHttpControllerTypeResolver : DefaultHttpControllerTypeResolver
+    {
+        public CustomHttpControllerTypeResolver()
+            : base(IsHttpEndpoint)
+        { }
+
+        internal static bool IsHttpEndpoint(Type type)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+
+            return type.IsClass 
+                  && type.IsVisible 
+                  && !type.IsAbstract
+                  && typeof(ApiController).IsAssignableFrom(type) 
+                  && typeof(IHttpController).IsAssignableFrom(type);
+        }
+    }
+}
